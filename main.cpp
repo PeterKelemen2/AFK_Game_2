f63013541f18200be2f66ec8d20798f232d0784c
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2022-04-13 00:05:38 +0300

    Commit #2

--- main.cpp
+++ main.cpp
@@ -1,41 +1,99 @@
 #include <Windows.h>
 #include <iostream>
 #include <conio.h>
+#include <iomanip>
 
 using namespace std;
 
 // Declaration of variables
 void time_flowing();
 void stuff_happening();
-void print_stuff();
+void print_scoreboard();
 void options();
+void top_bottom();
+void spaces();
+void print_score();
+void UI();
 
 // Declaration of functions
 long long int score = 10;
-int add = 10;
+int add = 5000;
+int length = 1;
+
+void UI(){
+    HWND console = GetConsoleWindow();
+  RECT ConsoleRect;
+  GetWindowRect(console, &ConsoleRect);
+
+   MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 500, 200, TRUE);
+
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = 70;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
 
 void time_flowing(){
     if(kbhit()){ // Core
         char x = getch(); // waiting for the input
-        options(); // Printing the options
+       // options(); // Printing the options
             }
-    Sleep(1000); // Same
+    score = score + add;
 }
 
-void print_stuff(){
-    cout << "+------------------+" << endl;
-    cout << "|                  |" << endl;
-    cout << setw(20) << "| Score: " << score << "|" << endl;
-    cout << "|                  |" << endl;
-    cout << "+------------------+" << endl;
+void top_bottom(){
+    // "+--------------+" 9 + length + 2
+        cout << "+";
+    for(int i=0; i < 9+length; i++)
+        cout << "-";
+        cout << "+";
+    cout << endl;
+}
+
+void spaces(){
+    // "|            |"
+        cout << "|";
+    for(int i=0; i < 9+length; i++)
+        cout << " ";
+        cout << "|";
+    cout << endl;
+}
+
+void print_score(){
+    spaces();
+    // "| Score: "(9 spaces) + length + " |" (2 spaces);
+        cout << "| Score: " << score << " |" << endl;
+    spaces();
+}
+
+void print_scoreboard(){
+    long long aux = score ;
+    length = 0;
+
+    while(aux > 0){ // Getting score length everytime eeeeh
+        aux = aux/10;
+        length ++;
+    }
+
+    top_bottom();
+    print_score();
+    top_bottom();
 }
 
 int main ()
 {
+    UI();
+
     for(;;){
         system("CLS");
-        print_stuff();
-        time_flowing;
-        print_stuff();
+
+        print_scoreboard();
+        time_flowing();
+        Sleep(1000);
     }
 }
